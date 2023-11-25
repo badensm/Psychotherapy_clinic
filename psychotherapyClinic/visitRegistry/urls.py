bae9aba2dad40e51ab7ff7f1cb3b8ac5d705f9f5
Marcin <badenski.marcin@gmail.com>
2023-11-25 16:12:54 +0100

    lita wizyt, dane usera

--- psychotherapyClinic/visitRegistry/urls.py
+++ psychotherapyClinic/visitRegistry/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('account/', views.account, name='account'),
-    path('visit_list', views.visit_list, name='visit_list')
+    path('account/<int:user_id>/', views.account, name='account'),
+    path('visit/list', views.visit_list, name='visit_list'),
+    path('visit/detail/<int:visit_id>', views.visit_detail, name='visit_detail')
 ]
--- psychotherapyClinic/visitRegistry/views.py
+++ psychotherapyClinic/visitRegistry/views.py
@@ -1,10 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
+from .models import Therapist, Patient, Visit
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request,'home.html')
 
-def account(request):
-    return render(request,'account.html')
+@login_required (login_url='login_user') 
+def account(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if user.is_staff:
+        additional_data = get_object_or_404(Therapist, user_id=user_id)
+    else:
+        additional_data = get_object_or_404(Patient, user_id=user_id)
+    return render(request,'account.html',{'user': user, 'additional_data': additional_data})
 
+@login_required (login_url='login_user') 
 def visit_list(request):
-    return render(request,'visit_list.html')
+    visits = Visit.objects.all()
+    return render(request,'visit_list.html',{'visits': visits})
+
+@login_required (login_url='login_user') 
+def visit_detail(request, visit_id):
+    visit = get_object_or_404(Visit, id=visit_id)
+    return render(request,'visit_detail.html')
